bc76bf334efa987b84d46cc2d468d4cc4cb74cba
xarn0 <xouli@list.ru>
2025-10-09 02:57:30 +0300

    943

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,4 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
 	compatibilityDate: "2025-05-15",
@@ -10,35 +10,24 @@ export default defineNuxtConfig({
 		pageTransition: { name: "page", mode: "out-in" }
 	},
 	runtimeConfig: {
-		WOO_TOKEN: "ck_8aa0d73a4771f95af2847d3357f8b53cc6cf749e", // будет доступно только на сервере
-		WOO_SECRET: "cs_4ee3b9424cab2f03b43808d3d82cf3fd162ddd6d",
-
+		WOO_TOKEN: process.env.WOO_TOKEN || "",
+		WOO_SECRET: process.env.WOO_SECRET || "",
 		public: {
 			apiBase: "http://nuxt/"
-		} // public-переменные, если нужно с клиента
+		}
 	},
-	modules: ["@prisma/nuxt", "@pinia/nuxt"],
-	// auth: {
-	// 	origin: "http://localhost:3000",
-	// 	enableGlobalAppMiddleware: true
-	// },
+	modules: ["@pinia/nuxt"], // убрали @prisma/nuxt
 	experimental: {
 		componentIslands: true
 	},
-	prisma: { autoSetupPrisma: true },
 	css: ["vuetify/styles"],
 	devtools: {
 		enabled: true,
-
-		timeline: {
-			enabled: true
-		}
+		timeline: { enabled: true }
 	},
-
 	build: {
 		transpile: ["vuetify"]
 	},
-
 	vite: {
 		vue: {
 			template: {
@@ -47,6 +36,11 @@ export default defineNuxtConfig({
 					includeAbsolute: false
 				}
 			}
+		}
+	},
+	nitro: {
+		externals: {
+			inline: ["@prisma/client"] // Prisma не пакуется в фронтенд
 		}
 	}
 });
